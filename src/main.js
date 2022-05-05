5a7452c182811e0c72736f324d79adf150a3def9
Alex Qin <qianjin.qin@qq.com>
2022-05-05 10:33:58 +0800

    add tailwind css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  <h1>Test Netlify!</h1>
+  <h1>Hello Netlify!</h1>
+  <h1 class="text-3xl font-bold underline">Test tailwindcss!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
