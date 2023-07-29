f2b59b2815d78ad247849b561dd9827a79d43faf
Alex Qin <qianjin.qin@qq.com>
2023-07-29 19:41:19 +0800

    update the nav

--- main.js
+++
@@ -1,8 +0,0 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <h1>Hello Netlify!</h1>
-  <h1 class="text-3xl font-bold underline">Test tailwindcss!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./**/*.{vue,js,ts,jsx,tsx}",
+    "./js/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
